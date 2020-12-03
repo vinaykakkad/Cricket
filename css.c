07c628a00aa5d3c9d2723db5460f8da31381b584
vinay kakkad <imkkad@gmail.com>
2020-12-03 12:28:16 +0530

    Bug fixes and formatting

--- css.c
+++ css.c
@@ -1,45 +1,41 @@
-
-#include"viewRecords1.h"
-#include"startMatch1.h"
+#include "viewRecords.h"
+#include "startMatch.h"
 
 void start()
 {
-    int r=0; 
+    int r = 0;
 
     system("cls");
     system("color 03");
-    
+
     printf("******************\n        Press\n\n1 To view records\n2 To start a match\n3 To exit\n******************\n\n");
     do
     {
         fflush(stdin);
-        scanf("%d",&r);
-        
+        scanf("%d", &r);
+
         switch (r)
         {
-            case 1:
-                system("cls");
-                viewRecords();
-                break;
-            case 2:
-                system("cls");
-                startMatch();
-                break;
-            case 3:
-                exit;
-                break;
-            default:
-                printf("You have entered wrong number!!!!\nPlease enter again.\n\n");
-                break;
+        case 1:
+            system("cls");
+            viewRecords();
+            break;
+        case 2:
+            system("cls");
+            startMatch();
+            break;
+        case 3:
+            exit;
+            break;
+        default:
+            printf("You have entered wrong number!!!!\nPlease enter again.\n\n");
+            break;
         }
-            
-    }
-    while(r!=1 && r!=2 && r!=3);        
-}
 
+    } while (r != 1 && r != 2 && r != 3);
+}
 
 void main()
 {
-    start();   
-}    
-
+    start();
+}
---
+++ startMatch.h
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+typedef struct date
+{
+    int d, m, y;
+} date;
+
+typedef struct player
+{
+    char name[30];
+    char outType[10];
+    int out, inAt, overs;
+    int batRuns, batBalls, fours, sixes;
+    int bowlRuns, bowlBalls, wickets, extras /*,maidens*/;
+} player;
+
+typedef struct team
+{
+    char name[30];
+    int balls, runs, wickets, order;
+    player players[11];
+
+} team;
+
+typedef struct match
+{
+    team battingTeam, bowlingTeam;
+    char venue[30];
+    date match_date;
+    int overs;
+    char toss[30];
+    char decision[5];
+    int innings;
+    player *onStrike;
+    player *offStrike;
+    player *currentBowler;
+} match;
+
+void start();
+
+void startMatch();
+
+void viewTeams(match *);
+
+void afterViewing(match *);
+
+void editTeam(match *);
+
+void toss(match *);
+
+void options();
+
+void rules(match *);
+
+void scoringMain(match *);
+
+void changeStrike(player *, player *);
+
+void changeTeam(team *, team *);
+
+void runs(match *);
+
+void nb(match *);
+
+void wide(match *);
+
+void blb(match *);
+
+void w(match *);
+
+void dirDismissal(match *);
+
+void runOut(match *);
+
+void startScoring(match *);
+
+void innings(match *);
+
+void ball(match *);
+
+void Scorecard(match *, FILE *);
+
+void matchEnd(match *);
+
+player *nextBatsman(player *, match *);
+
+player *nextBowler(match *);
+
+void Summary(match *);
+
+void storeScore(match *);
